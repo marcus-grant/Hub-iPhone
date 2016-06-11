1ddba71e44946571b57d954b9cdfa44056e2f56f
Marcus Grant <marcusfg@gmail.com>
2016-06-10 22:45:20 -0400

    first commit with skeleton tab storyboard and core data stack

---
+++ Hub-iPhone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hub-iPhone
+//
+//  Created by Marcus Grant on 6/10/16.
+//  Copyright © 2016 Marcus Grant. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+## Hub-iPhone
+
+The public portion of my upcoming app whose details will become more clear as the first release hits the app store. Much of the "special sauce" is hidden away with the .gitignore to save some of the more marketable features it will include. For now I'll say that this app will be a way to bind together a lot of communication platforms into one gateway app, this one.
